eead8dee9b1f8d89a5742dbb7103698958cd2857
munkovar <munkovar@gmail.com>
2022-02-04 13:07:48 +0100

    initial commit

---
+++ src/com/ukol1/Main.java
@@ -0,0 +1,20 @@
+package com.ukol1;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name;
+        String surname;
+        int numberOfPurchase;
+        String town;
+        double fuel;
+        double lenghtOfCable;
+        LocalDate dateOfBirth;
+        String spz;
+
+
+    }
+}
