402bee95f29485475962a7a7792475ef280f6a41
munkovar <munkovar@gmail.com>
2022-02-04 13:35:55 +0100

    initial commit

--- src/com/ukol1/Main.java
+++ src/com/ukol1/Main.java
@@ -14,7 +14,7 @@ public class Main {
         double lenghtOfCable = 716.55;
         LocalDate dateOfBirth = LocalDate.of(1980, 3,3);
         String spz = "1T6 6132";
-        String ip = "192.168.1.1";
+        String ip = "192.168.1.1"; //
 
     System.out.println(name+"\n"+surname+"\n"+numberOfPurchase+"\n"+town+"\n"+fuel+"\n"+lenghtOfCable+"\n"+dateOfBirth+"\n"+spz+"\n"+ip );
     }
