6c9fa0e03c98603342546160f9eb176c33085379
munkovar <munkovar@gmail.com>
2022-02-04 13:29:04 +0100

    initial commit

--- src/com/ukol1/Main.java
+++ src/com/ukol1/Main.java
@@ -6,15 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String name;
-        String surname;
-        int numberOfPurchase;
-        String town;
-        double fuel;
-        double lenghtOfCable;
-        LocalDate dateOfBirth;
-        String spz;
+        String name = "Jan";
+        String surname = "Novák";
+        int numberOfPurchase = 10;
+        String town = "Ostrava";
+        double fuel = 8.75;
+        double lenghtOfCable = 716.55;
+        LocalDate dateOfBirth = LocalDate.of(1980, 3,3);
+        String spz = "1T6 6132";
+        String ip = "192.168.1.1";
 
-
+    System.out.println(name+"\n"+surname+"\n"+numberOfPurchase+"\n"+town+"\n"+fuel+"\n"+lenghtOfCable+"\n"+dateOfBirth+"\n"+spz+"\n"+ip );
     }
 }
